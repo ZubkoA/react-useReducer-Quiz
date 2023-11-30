fb74350d6bc90771c851f94d88ceefe0353406bc
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-11-30 16:08:53 +0200

    create repository, add gh-page

---
+++ src/components/Progress.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Progress = ({ index, numQuestions, points, maxPoints, answer }) => {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question {index + 1} / {numQuestions}
+      </p>
+      <p>
+        {points}/{maxPoints}
+      </p>
+    </header>
+  );
+};
+
+export default Progress;
